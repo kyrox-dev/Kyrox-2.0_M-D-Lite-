e05e59c278f6f7f1232733082a5f7eba154477d0
kyrox-dev <zamybmk@gmail.com>
2025-08-03 12:34:40 -0400

    index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Bot actif');
+});
+
 app.get('/pair', (req, res) => {
   res.send('Page Pair active');
 });
