f0f536511c72271fb721e5703e392194510f160a
kyrox-dev <zamybmk@gmail.com>
2025-08-03 19:25:45 -0400

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-```js
+
 const { default: makeWASocket, useSingleFileAuthState } = require('@adiwajshing/baileys')
-const express = require('express')
+const express = require(express)
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -15,25 +15,24 @@ async function startBot() {
     printQRInTerminal: false,
   })
 
-  sock.ev.on('connection.update', update => {
+  sock.ev.on(connection.update, update => {
     if (update.qr) {
       qr = update.qr
     }
   })
 
-  sock.ev.on('creds.update', saveState)
+  sock.ev.on(creds.update, saveState)
 
   app.get('/qr', (_, res) => {
-    app.get('/qr', (_, res) => {
   if (qr) {
-    res.send(`<img src="https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qr)}" />`)
+    res.send(<img src="https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qr)}" />)
   } else {
-    res.send('QR code not available yet')
+    res.send(QR code not available yet)
   }
 })
 
-  app.listen(PORT, () => console.log(`Server running on port{PORT}`))
+  app.listen(PORT, () => console.log(Server running on port{PORT}))
 }
 
 startBot()
-```
+
