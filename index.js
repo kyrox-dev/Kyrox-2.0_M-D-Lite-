a42d4461c9115f73a2a0b67459383a60b2ab39ba
kyrox-dev <zamybmk@gmail.com>
2025-08-03 10:56:29 -0400

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+
+const express = require('express');
+const app = express();
+
 app.get('/pair', (req, res) => {
   res.send('Page Pair active');
+});
+
+app.listen(8000, 'localhost', () => {
+  console.log('Server running on http://localhost:8000');
 });
