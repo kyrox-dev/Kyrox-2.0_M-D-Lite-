8e9d792d0be461a45ff0fb0ea65d80164a151793
kyrox-dev <zamybmk@gmail.com>
2025-08-04 07:58:36 -0400

    pair.js

--- pair.js
+++ pair.js
@@ -1,9 +1,10 @@
-
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Page Pair active');
-});
-
-module.exports = router;
+function makeid(num = 4) {
+  let result = "";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var characters9 = characters.length;
+  for (var i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters9));
+  }
+  return result;
+}
+module.exports = {makeid};
