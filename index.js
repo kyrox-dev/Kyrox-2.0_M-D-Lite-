3f0a6ca2a2943c00486b3511c4e0823257cc5bf5
kyrox-dev <zamybmk@gmail.com>
2025-08-04 00:28:47 -0400

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+const fs = require('fs')
+const path = require('path')
 
+(function () {
+  const _0xk1 = Buffer.from('S3lyb3gtMi4wX00tRC1MaXRl').toString('utf8')
+  const _0xk2 = 'S1lSMFhfTE9DSw=='
+  const _0xk3 = Buffer.from(_0xk2, 'base64').toString('utf8')
+
+  const _0xcheck = (a, b) => {
+    if (a !== _0xk1 || b !== _0xk3) {
+      console.log('\x1b[31m%s\x1b[0m', '❌ Code compromis ou bot non autorisé.')
+      // Supprimer des fichiers clés (exemple)
+      try {
+        fs.unlinkSync(path.join(__dirname, 'index.js'))
+        fs.unlinkSync(path.join(__dirname, 'package.json'))
+      } catch {}
+      // Quitter l'app
+      process.exit(1)
+    }
+  }
+
+  _0xcheck(_0xk1, _0xk3)
+})()
 const { default: makeWASocket, useSingleFileAuthState } = require('@adiwajshing/baileys')
 const express = require(express)
 
